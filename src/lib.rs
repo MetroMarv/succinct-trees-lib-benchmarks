731db1c708dc6160d2d4d0b2f8e1ac2bf6734e7f
Marvin Rensing <marvin.rensing@posteo.de>
2018-07-16 21:58:33 +0200

    More benchmarks and results

--- src/lib.rs
+++ src/lib.rs
@@ -10,80 +10,14 @@ mod tests {
     use succinct_trees_lib::succinct_trees;
     use succinct_trees_lib::succinct_trees::parser::TreeParser;
     use succinct_trees_lib::succinct_trees::SuccinctTreeFunctions;
-    use bv::{Bits,BitVec};
     use std::fs::File;
     use test::Bencher;
 
-    #[bench]
-    fn bench_read_bp_10000_nodes(b: &mut Bencher) {
-        let path = String::from("resources/parenthesis_bp_nodes_7691_depth_5_max_children_10.txt");
-        let file = File::open(path).unwrap();
 
-        let mut parser = TreeParser::new(file);
 
-        b.iter(|| {
-            let bp = parser.read_bp();
-        });
-    }
-
-    #[bench]
-    fn bench_read_louds_10000_nodes(b: &mut Bencher) {
-        let path = String::from("resources/parenthesis_louds_nodes_7691_depth_5_max_children_10.txt");
-        let file = File::open(path).unwrap();
-
-        let mut parser = TreeParser::new(file);
-
-        b.iter(|| {
-            let bp = parser.read_louds();
-        });
-    }
-
-    #[bench]
-    fn bench_read_bp_27_nodes(b: &mut Bencher) {
-        let path = String::from("resources/parenthesis_bp_nodes_27_depth_10_max_children_2.txt");
-        let fileResult= File::open(path);
-
-        let file = match fileResult {
-            Ok(file) => file,
-            Err(err) => panic!("My Error. {:?}", err)
-        };
-
-        let mut parser = TreeParser::new(file);
-
-        b.iter(|| {
-            let bp = parser.read_bp();
-        });
-    }
-
-    #[bench]
-    fn bench_read_louds_27_nodes(b: &mut Bencher) {
-        let path = String::from("resources/parenthesis_louds_nodes_27_depth_10_max_children_2.txt");
-        let file = File::open(path).unwrap();
-
-        let mut parser = TreeParser::new(file);
-
-        b.iter(|| {
-            let bp = parser.read_louds();
-        });
-    }
-
-    fn get_read_louds_tree_27() -> succinct_trees::louds::Louds{
-        let path = String::from("resources/parenthesis_louds_nodes_27_depth_10_max_children_2.txt");
-        let file = File::open(path).unwrap();
-
-        let mut parser = TreeParser::new(file);
-
-        parser.read_louds()
-    }
-
-    fn get_read_louds_tree_200() -> succinct_trees::louds::Louds{
-        let path = String::from("resources/parenthesis_louds_nodes_194_depth_5_max_children_5.txt");
-        let file = File::open(path).unwrap();
-
-        let mut parser = TreeParser::new(file);
-
-        parser.read_louds()
-    }
+    /**
+     * Benchmarks with approx. 1600 nodes tree
+     */
 
     fn get_read_louds_tree_1600() -> succinct_trees::louds::Louds{
         let path = String::from("resources/parenthesis_louds_nodes_1670_depth_10_max_children_3.txt");
@@ -94,25 +28,364 @@ mod tests {
         parser.read_louds()
     }
 
+    fn get_read_bp_tree_1600() -> succinct_trees::bp::BalancedParenthesis{
+        let path = String::from("resources/parenthesis_bp_nodes_1670_depth_10_max_children_3.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        parser.read_bp()
+    }
+
+    #[bench]
+    fn bench_1600_louds_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_louds_nodes_1670_depth_10_max_children_3.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        b.iter(|| {
+            parser.read_louds();
+        });
+    }
+
+    #[bench]
+    fn bench_1600_bp_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_bp_nodes_1670_depth_10_max_children_3.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        b.iter(|| {
+            parser.read_bp();
+        });
+    }
+
+    #[bench]
+    fn bench_1600_louds_is_leaf(b: &mut Bencher) {
+        let tree = get_read_louds_tree_1600();
+
+        b.iter(|| {
+            tree.is_leaf(1600);
+        })
+    }
+
+    #[bench]
+    fn bench_1600_bp_is_leaf(b: &mut Bencher) {
+        let tree = get_read_bp_tree_1600();
+
+        b.iter(|| {
+            tree.is_leaf(1600);
+        })
+    }
+
+    #[bench]
+    fn bench_1600_louds_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_1600();
+
+        b.iter(|| {
+            tree.parent(1600);
+        })
+    }
+
+    #[bench]
+    fn bench_1600_bp_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_1600();
+
+        b.iter(|| {
+            tree.parent(1600);
+        })
+    }
+
+    #[bench]
+    fn bench_1600_louds_first_child(b: &mut Bencher) {
+        let tree = get_read_louds_tree_1600();
+
+        b.iter(|| {
+            tree.first_child(1600);
+        })
+    }
+
+    #[bench]
+    fn bench_1600_bp_first_child(b: &mut Bencher) {
+        let tree = get_read_bp_tree_1600();
+
+        b.iter(|| {
+            tree.first_child(1600);
+        })
+    }
+
+    #[bench]
+    fn bench_1600_louds_next_sibling(b: &mut Bencher) {
+        let tree = get_read_louds_tree_1600();
+
+        b.iter(|| {
+            tree.next_sibling(1600);
+        })
+    }
+
+    #[bench]
+    fn bench_1600_bp_next_sibling(b: &mut Bencher) {
+        let tree = get_read_bp_tree_1600();
+
+        b.iter(|| {
+            tree.next_sibling(1600);
+        })
+    }
+
+    /**
+     * Benchmarks with approx. 8000 nodes tree
+     */
+
+
     fn get_read_louds_tree_8000() -> succinct_trees::louds::Louds{
         let path = String::from("resources/parenthesis_louds_nodes_7691_depth_5_max_children_10.txt");
         let file = File::open(path).unwrap();
 
         let mut parser = TreeParser::new(file);
 
-        parser.read_louds()
+        parser.read_louds_blksize(256)
     }
 
-    fn get_read_louds_tree_100000() -> succinct_trees::louds::Louds{
+    fn get_read_bp_tree_8000() -> succinct_trees::bp::BalancedParenthesis{
+        let path = String::from("resources/parenthesis_bp_nodes_7691_depth_5_max_children_10.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        parser.read_bp()
+    }
+
+    #[bench]
+    fn bench_8000_louds_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_louds_nodes_7691_depth_5_max_children_10.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        b.iter(|| {
+            parser.read_louds();
+        });
+    }
+
+    #[bench]
+    fn bench_8000_bp_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_bp_nodes_7691_depth_5_max_children_10.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        b.iter(|| {
+            parser.read_bp();
+        });
+    }
+
+    #[bench]
+    fn bench_8000_louds_is_leaf(b: &mut Bencher) {
+        let tree = get_read_louds_tree_8000();
+
+        b.iter(|| {
+            tree.is_leaf(8000);
+        })
+    }
+
+    #[bench]
+    fn bench_8000_bp_is_leaf(b: &mut Bencher) {
+        let tree = get_read_bp_tree_8000();
+
+        b.iter(|| {
+            tree.is_leaf(8000);
+        })
+    }
+
+    #[bench]
+    fn bench_8000_louds_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_8000();
+
+        b.iter(|| {
+            tree.parent(4000);
+        })
+    }
+
+    #[bench]
+    fn bench_8000_bp_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_8000();
+
+        b.iter(|| {
+            tree.parent(4000);
+        })
+    }
+
+    #[bench]
+    fn bench_8000_louds_first_child(b: &mut Bencher) {
+        let tree = get_read_louds_tree_8000();
+
+        b.iter(|| {
+            tree.first_child(8000);
+        })
+    }
+
+    #[bench]
+    fn bench_8000_bp_first_child(b: &mut Bencher) {
+        let tree = get_read_bp_tree_8000();
+
+        b.iter(|| {
+            tree.first_child(8000);
+        })
+    }
+
+    #[bench]
+    fn bench_8000_louds_next_sibling(b: &mut Bencher) {
+        let tree = get_read_louds_tree_8000();
+
+        b.iter(|| {
+            tree.next_sibling(8000);
+        })
+    }
+
+    #[bench]
+    fn bench_8000_bp_next_sibling(b: &mut Bencher) {
+        let tree = get_read_bp_tree_8000();
+
+        b.iter(|| {
+            tree.next_sibling(8000);
+        })
+    }
+
+    /**
+     * Benchmarks with approx. 65000 nodes tree
+     */
+
+    fn get_read_louds_tree_65000() -> succinct_trees::louds::Louds{
+        let path = String::from("resources/parenthesis_louds_nodes_65726_depth_7_max_children_10.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        parser.read_louds_blksize(2048)
+    }
+
+    fn get_read_bp_tree_65000() -> succinct_trees::bp::BalancedParenthesis{
+        let path = String::from("resources/parenthesis_bp_nodes_65726_depth_7_max_children_10.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        parser.read_bp()
+    }
+
+    #[bench]
+    fn bench_65000_louds_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_louds_nodes_65726_depth_7_max_children_10.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        b.iter(|| {
+            parser.read_louds();
+        });
+    }
+
+    #[bench]
+    fn bench_65000_bp_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_bp_nodes_65726_depth_7_max_children_10.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        b.iter(|| {
+            parser.read_bp();
+        });
+    }
+
+    #[bench]
+    fn bench_65000_louds_is_leaf(b: &mut Bencher) {
+        let tree = get_read_louds_tree_65000();
+
+        b.iter(|| {
+            tree.is_leaf(65000);
+        })
+    }
+
+    #[bench]
+    fn bench_65000_bp_is_leaf(b: &mut Bencher) {
+        let tree = get_read_bp_tree_65000();
+
+        b.iter(|| {
+            tree.is_leaf(65000);
+        })
+    }
+
+    #[bench]
+    fn bench_65000_louds_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_65000();
+
+        b.iter(|| {
+            tree.parent(65000);
+        })
+    }
+
+    #[bench]
+    fn bench_65000_bp_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_65000();
+
+        b.iter(|| {
+            tree.parent(65000);
+        })
+    }
+
+    #[bench]
+    fn bench_65000_louds_first_child(b: &mut Bencher) {
+        let tree = get_read_louds_tree_65000();
+
+        b.iter(|| {
+            tree.first_child(65000);
+        })
+    }
+
+    #[bench]
+    fn bench_65000_bp_first_child(b: &mut Bencher) {
+        let tree = get_read_bp_tree_65000();
+
+        b.iter(|| {
+            tree.first_child(65000);
+        })
+    }
+
+    #[bench]
+    fn bench_65000_louds_next_sibling(b: &mut Bencher) {
+        let tree = get_read_louds_tree_65000();
+
+        b.iter(|| {
+            tree.next_sibling(65000);
+        })
+    }
+
+    #[bench]
+    fn bench_65000_bp_next_sibling(b: &mut Bencher) {
+        let tree = get_read_bp_tree_65000();
+
+        b.iter(|| {
+            tree.next_sibling(65000);
+        })
+    }
+
+    /**
+     * Benchmarks with 100.000 nodes
+     */
+
+    fn get_read_louds_tree_120000() -> succinct_trees::louds::Louds{
         let path = String::from("resources/parenthesis_louds_nodes_128457_depth_10_max_children_5.txt");
         let file = File::open(path).unwrap();
 
         let mut parser = TreeParser::new(file);
 
-        parser.read_louds()
+        parser.read_louds_blksize(4096)
     }
 
-    fn get_read_bp_tree_100000() -> succinct_trees::bp::BalancedParenthesis{
+
+
+    fn get_read_bp_tree_120000() -> succinct_trees::bp::BalancedParenthesis{
         let path = String::from("resources/parenthesis_bp_nodes_128457_depth_10_max_children_5.txt");
         let file = File::open(path).unwrap();
 
@@ -122,93 +395,98 @@ mod tests {
     }
 
     #[bench]
-    fn bench_louds_is_leaf_100000(b: &mut Bencher) {
-        let tree = get_read_louds_tree_100000();
+    fn bench_120000_louds_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_louds_nodes_128457_depth_10_max_children_5.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
 
         b.iter(|| {
-            tree.is_leaf(10000);
+            parser.read_louds();
+        });
+    }
+
+    #[bench]
+    fn bench_120000_bp_read(b: &mut Bencher) {
+        let path = String::from("resources/parenthesis_bp_nodes_128457_depth_10_max_children_5.txt");
+        let file = File::open(path).unwrap();
+
+        let mut parser = TreeParser::new(file);
+
+        b.iter(|| {
+            parser.read_bp();
+        });
+    }
+
+    #[bench]
+    fn bench_120000_louds_is_leaf(b: &mut Bencher) {
+        let tree = get_read_louds_tree_120000();
+
+        b.iter(|| {
+            tree.is_leaf(120000);
         })
     }
 
     #[bench]
-    fn bench_bp_is_leaf_100000(b: &mut Bencher) {
-        let tree = get_read_bp_tree_100000();
+    fn bench_120000_bp_is_leaf(b: &mut Bencher) {
+        let tree = get_read_bp_tree_120000();
 
         b.iter(|| {
-            tree.is_leaf(100000);
+            tree.is_leaf(120000);
         })
     }
 
     #[bench]
-    fn bench_louds_parent_27(b: &mut Bencher) {
-        let tree = get_read_louds_tree_27();
+    fn bench_120000_louds_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_120000();
 
         b.iter(|| {
-            tree.parent(36);
+            tree.parent(120000);
         })
     }
 
     #[bench]
-    fn bench_louds_parent_200(b: &mut Bencher) {
-        let tree = get_read_louds_tree_200();
-
-        for i in 8..388 {
-            tree.parent(i);
-            println!("Success for {}", i);
-        }
-    }
-
-    #[bench]
-    fn bench_louds_parent_1600(b: &mut Bencher) {
-        let tree = get_read_louds_tree_1600();
+    fn bench_120000_bp_parent(b: &mut Bencher) {
+        let tree = get_read_louds_tree_120000();
 
         b.iter(|| {
-            tree.parent(1700);
+            tree.parent(120000);
         })
     }
 
     #[bench]
-    fn bench_louds_parent_8000(b: &mut Bencher) {
-        let tree = get_read_louds_tree_8000();
+    fn bench_120000_louds_first_child(b: &mut Bencher) {
+        let tree = get_read_louds_tree_120000();
 
         b.iter(|| {
-            tree.parent(4000);
+            tree.first_child(118000);
         })
     }
 
     #[bench]
-    fn bench_louds_parent_100000(b: &mut Bencher) {
-        let tree = get_read_louds_tree_100000();
+    fn bench_120000_bp_first_child(b: &mut Bencher) {
+        let tree = get_read_bp_tree_120000();
 
         b.iter(|| {
-            tree.parent(100000);
+            tree.first_child(120000);
         })
     }
 
     #[bench]
-    fn bench_bp_parent_100000(b: &mut Bencher) {
-        let tree = get_read_bp_tree_100000();
+    fn bench_120000_louds_next_sibling(b: &mut Bencher) {
+        let tree = get_read_louds_tree_120000();
 
         b.iter(|| {
-            tree.parent(99988);
+            tree.next_sibling(120000);
         })
     }
 
     #[bench]
-    fn bench_louds_first_child_100000(b: &mut Bencher) {
-        let tree = get_read_louds_tree_100000();
+    fn bench_120000_bp_next_sibling(b: &mut Bencher) {
+        let tree = get_read_bp_tree_120000();
 
         b.iter(|| {
-            tree.first_child(100000);
-        })
-    }
-
-    #[bench]
-    fn bench_bp_first_child_100000(b: &mut Bencher) {
-        let tree = get_read_bp_tree_100000();
-
-        b.iter(|| {
-            tree.first_child(99988);
+            tree.next_sibling(120000);
         })
     }
 }
